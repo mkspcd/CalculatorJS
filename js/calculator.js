d039fcef1542b5e6c823d938491d3fb69a9cb318
mkspcd <mkspcd@gmail.com>
2017-04-02 10:30:00 +0200

    Added input control when clicking the  '.' button
    Verification if the '.' should be added whan the user clicks the '.'
    button. For example, it's not added if the operation already has a '.'
    as last character, or if the last number is already decimal.
    
    Small fixes

--- js/calculator.js
+++ js/calculator.js
@@ -1,24 +1,27 @@
 /*
 Placeholders pour les champs qui affichent l'operation et le resultat.
-La variable visualOperation est une chaine de caractères qui sera affichée à l'utilisateur, en haut de la calculatrice.
-La variable operation est ce qui sera evalué avec la fonction eval()
+La variable 'visualOperation' est une chaine de caractères qui sera affichée à l'utilisateur, en haut de la calculatrice.
+La variable 'operation' est ce qui sera evalué avec la fonction eval()
 On a besoin de les stocker dans des variables différentes car les signes affichés sont différents des signent qui servent à évaluer (le signe diviser '÷' par exemple)
 */
 var visualOperation = document.getElementById("calculator-screen-operation");
 visualOperation.innerHTML = "0";
 var operation = "";
 
+/* Le resultat qui sera affiché sur l'ecran de la calculatrice */
 var result = document.getElementById("calculator-screen-result");
 result.innerHTML = "0";
 
 /* On reset la calculatrice quand l'utilisateur clique sur le resultat */
 result.onclick = function() { clear(); };
 
+/* On stocke certains element (les boutons des operateurs) dans des variables  */
 var divide = document.getElementById("divide");
 var multiply = document.getElementById("multiply");
 var minus = document.getElementById("minus");
 var plus = document.getElementById("plus");
 
+/* Adding corresponding events when the user clicks an element */
 divide.onclick = function() {
 	if (endsWithOperator()) {
 		visualOperation.innerHTML = visualOperation.innerHTML.substring(0, visualOperation.innerHTML.length - 1) + "÷";
@@ -73,34 +76,58 @@ document.getElementById("num7").onclick = function() { concatNumber(7); };
 document.getElementById("num8").onclick = function() { concatNumber(8); };
 document.getElementById("num9").onclick = function() { concatNumber(9); };
 
+/*
+Quand l'utilisateur clique sur le point '.' : 
+	- si le signe precedent est un operateur, on ajoute un zero devant le point,
+	- si les dernier nombre de la chaine est deja decimal, on ne fait rien
+	- sinon, on ajoute le point
+*/
 document.getElementById("dot").onclick = function() {
-	visualOperation.innerHTML += ".";
-	operation += ".";
-	evaluate();
+	if ( endsWithOperator() ) {
+		visualOperation.innerHTML += "0.";
+		operation += "0.";
+		evaluate();
+	} else if ( !lastNumberisDecimal() ) {
+		visualOperation.innerHTML += ".";
+		operation += ".";
+		evaluate();
+	}	
 };
 
+/* Ajout des chiffres cliqués à la chaine representant l'operation. */
 function concatNumber(x) {
-	updateVisualOperation(x);
-	operation += x;
-	evaluate();
+	if ( !isTooLong() ) {
+		updateVisualOperation(x);
+		operation += x;
+		evaluate();
+	}
 }
 
 function evaluate() {
 	result.innerHTML = eval(operation);
 	result.innerHTML = result.innerHTML.substring(0, 8);
-	console.log( operation + " = " + eval(operation) );
 }
 
 function updateVisualOperation(x) {
 	visualOperation.innerHTML === "0" ? visualOperation.innerHTML = x : visualOperation.innerHTML += x;
 }
 
+/* Reset total de la calculatrice */
 function clear() {
 	visualOperation.innerHTML = "0";
 	operation = "";
 	result.innerHTML = "0";
 }
 
+/* On limite l'operation à 12 caractères */
+function isTooLong() {
+	if (visualOperation.innerHTML.length > 12) {
+		return true;
+	}
+	return false;
+}
+
+/* Fonction qui retourne true si le dernier caractere de 'operation' est un operateur */
 function endsWithOperator() {
 	if (operation.endsWith("/") || operation.endsWith("*") || operation.endsWith("-") || operation.endsWith("+")) {
 		return true;
@@ -108,14 +135,39 @@ function endsWithOperator() {
 	return false;
 }
 
+/* Fonction qui retourne true si le dernier nombre de 'operation' est decimal (et contient donc dejà un '.') */
+function lastNumberisDecimal() {
+	if (operation.indexOf(".") != -1) {
+		/*
+		Si 'operation' contient deja un '.', on verifie si celui-ci se trouve après le dernier operateur utilisé
+		*/
+		if (operation.lastIndexOf("/") < operation.lastIndexOf(".") || 
+			operation.lastIndexOf("*") < operation.lastIndexOf(".") || 
+			operation.lastIndexOf("-") < operation.lastIndexOf(".") || 
+			operation.lastIndexOf("+") < operation.lastIndexOf(".") ) {
+			return true;
+		}
+	}
+	return false;
+}
+
+/*
+Gestion du bouton 'C' pour corriger la frappe
+*/
 document.getElementById("correct").onclick = function() {
 	visualOperation.innerHTML = visualOperation.innerHTML.substring(0, visualOperation.innerHTML.length - 1);
 	operation = operation.substring(0, operation.length - 1);
 	
+	/* Si l'utilisateur appuie sur 'C' pour enlever tous les caracteres, on reset la calculatrice */
 	if (visualOperation.length == 0 || operation.length == 0) {
 		clear();
 	}
 	
+	/*
+	Après chaque appuie sur 'C', on met à jour le result :
+		- si operation ne se termine pas par un operateur, on evalue toute la chaine, 
+		- si a la suite d'un appuie sur 'C' le dernier caracere est un operateur, on evalue operation sans ce dernier operateur
+	*/
 	if (operation.length != 0 && !endsWithOperator()) {
 		evaluate();
 	}
--- readme.md
+++ readme.md
@@ -1,14 +1,18 @@
 # Calculator
 
-A minimal — but fully fonctionnal — calculator, written with **HTML**, **CSS** and **Javascript**.  
-It follows Google's [Material Design guidelines](https://material.io/guidelines/).
+A minimal — but fonctionnal — calculator, written with **HTML**, **CSS** and **JavaScript**.  
+It follows Google's [Material Design guidelines](https://material.io/guidelines/).  
+
+A click on the result resets the calculator.
 
 <p align="center">
   <img src="https://raw.githubusercontent.com/mkspcd/CalculatorJS/master/screenshot.png" alt="Calculator" />
 </p>
 
 Original design by *[Ruban Khalid](https://dribbble.com/shots/2321336-Calculator-004-Daily-UI)*.  
-I integrated his design and slightly modified it for my needs.
+I integrated his design and slightly modified it for my needs.  
+
+I used the [Materialize](http://materializecss.com/) framework only to add an effect when a button is clicked.
 
 
 
